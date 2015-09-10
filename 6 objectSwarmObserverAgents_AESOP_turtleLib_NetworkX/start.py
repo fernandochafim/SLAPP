ee554097a61ae006122e34093be0cc2fb8fe26a3
Pietro Terna <pietro.terna@unito.it>
2015-09-10 22:29:25 +0200

    Fixed again start.py

--- 6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX/start.py
+++ 6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX/start.py
@@ -9,7 +9,7 @@
 # project.txt in the folder "6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX"
 # the file has to contain the path and the name of the folder of the project
 
-print "SLAPP 1.01"
+print "SLAPP 1.1"
 import os
 confirm="n"
 found=False
