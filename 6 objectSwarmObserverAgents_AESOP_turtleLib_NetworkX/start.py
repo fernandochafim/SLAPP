388e6f9fa8c2a1a9dbe6f3374d75f9f8ab76bb3d
Pietro Terna <pietro.terna@unito.it>
2015-11-01 18:42:51 +0100

    Updated Reference Handbook.

--- 6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX/start.py
+++ 6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX/start.py
@@ -9,7 +9,7 @@
 # project.txt in the folder "6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX"
 # the file has to contain the path and the name of the folder of the project
 
-print "SLAPP 1.11 build 20151026"
+print "SLAPP 1.11 build 20151101"
 import os
 confirm="n"
 found=False
