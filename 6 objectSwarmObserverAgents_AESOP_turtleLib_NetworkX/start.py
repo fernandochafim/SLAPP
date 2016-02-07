791ef9ac3ba4f7feb3d74abfa29b76f96bcbaf95
Pietro Terna <pietro.terna@unito.it>
2016-02-07 22:29:38 +0100

    correcting a typo in a comment

--- 6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX/start.py
+++ 6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX/start.py
@@ -9,7 +9,7 @@
 # project.txt in the folder "6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX"
 # the file has to contain the path and the name of the folder of the project
 
-print "SLAPP 1.11 build 20160131"
+print "SLAPP 1.11 build 20160207"
 import os
 confirm="n"
 found=False
