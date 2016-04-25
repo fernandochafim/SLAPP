bd9df5e6c4ade6a017765f56449d91960dbf40f0
Pietro Terna <pietro.terna@unito.it>
2016-04-25 18:39:25 +0200

    considering new types of starting environment
    Now SLAPP works
    - from a plain terminal, with:
      python runShell.py
      OR
      ipython
      AND internally
      %run runShell.py
      (with the same graphic results of python in terminal)
    
    - from jupiter notebook (with inline graphics)
    
    - from Jupiter QtConsole—e.g. Anaconda launcher—-(with inline graphics)
    
    - from Spyder ipython (with inline graphics)

--- 6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX/$$slapp$$/graphicControl.py
+++ 6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX/$$slapp$$/graphicControl.py
@@ -45,9 +45,26 @@ def graphicControl():
   from IPython import get_ipython
   import matplotlib as mpl
 
-  # running in IPython with magic '%matplotlib|%pylab' already set,
-  # modified to '%matplotlib inline'
-  if get_ipython().config.has_key('InlineBackendConfig') and \
+  # check if running in a plain terminal with IPython starting with the
+  # 'ipython' command line OR in a jupyter notebook
+
+
+  # IN 'ipython' command line in a terminal, but the case of using it via
+  # Spyder, which is behaving as a Jupyter QtConsole (see below)
+  if "IPython.terminal.interactiveshell.TerminalInteractiveShell" \
+   in str(get_ipython()):
+
+   common.graphicStatus="PythonViaTerminal"
+   print 'SLAPP started from a terminal'
+
+
+  # IN a jupyter notebook OR in a Jupyter QtConsole (same effects)
+  if "ipykernel.zmqshell.ZMQInteractiveShell" \
+   in str(get_ipython()):
+
+   # running in IPython with magic '%matplotlib|%pylab' already set,
+   # modified to '%matplotlib inline'
+   if get_ipython().config.has_key('InlineBackendConfig') and \
       not "backend_inline" in mpl.get_backend():
       print "SLAPP running with magic '%matplotlib|%pylab' already set"
 
@@ -55,14 +72,14 @@ def graphicControl():
       print "'%matplotlib inline' magic command NOW SET"
       common.graphicStatus="%matplotlib inline"
 
-  # running in IPython with magic '%matplotlib inline|%pylab' already set
-  elif get_ipython().config.has_key('InlineBackendConfig') and \
+   # running in IPython with magic '%matplotlib inline|%pylab' already set
+   elif get_ipython().config.has_key('InlineBackendConfig') and \
       "backend_inline" in mpl.get_backend():
       print "running with magic '%matplotlib inline|%pylab inline' already set"
       common.graphicStatus="%matplotlib inline"
 
-  # running in IPython without any magic matplotlib already set
-  else:
+   # running in IPython without any magic matplotlib already set
+   else:
       print "SLAPP starting without any matplotlib magic command"
 
       get_ipython().magic("%matplotlib inline")
